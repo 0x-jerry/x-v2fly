364bc8f3d9578a3d7ecb2d5cde3797a6bb9c4233
Jerry Wang <x.jerry.wang@gmail.com>
2021-08-27 00:43:58 +0800

    chore: add install script

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # x-v2fly
+
+## install
+
+```sh
+curl https://raw.githubusercontent.com/0x-jerry/x-v2fly/main/install.sh | sh
+```
---
+++ install.sh
@@ -0,0 +1,1 @@
+deno install -r -qAf --no-check --unstable --import-map https://raw.githubusercontent.com/0x-jerry/x-v2fly/main/import_map.json https://raw.githubusercontent.com/0x-jerry/x-v2fly/main/x-v2fly.ts
